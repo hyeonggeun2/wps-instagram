ef4917d225c2d7b4176e1ae5bb9a2c4c082993f9
hyeonggeun2 <dhgudrms12@gmail.com>
2020-01-30 14:41:23 +0900

    poetry와 docker를 통해서 ec2를 동작할 수 있도록 설정

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -2,20 +2,34 @@
 
 IDENTIFY_FILE="$HOME/.ssh/nuna.pem"
 USER="ubuntu"
-IP="54.180.153.158"
+IP="13.124.45.97"
 HOST="${USER}@${IP}"
+ORIGIN_SOURCE="$HOME/projects/wps12th/instagram/"
+DOCKER_REPO="hyeonggeun2/wps-instagram"
 SSH_CMD="ssh -i ${IDENTIFY_FILE} ${HOST}"
+
 
 
 echo "== Docker 배포 =="
 
 echo "서버 업데이트"
-${SSH_CMD} sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y && apt -y autoremove
-echo "python3 pip 설치"
-${SSH_CMD} sudo apt -y install python3-pip
+${SSH_CMD} sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y && sudo apt -y autoremove
 
 echo "EC2에서 docker 설치"
-${SSH_CMD} sudo apt-get -y docker.io
+${SSH_CMD} sudo apt install -y docker.io
+
+echo "docker build"
+poetry export -f requirements.txt > requirements.txt
+docker build -q -t ${DOCKER_REPO} -f Dockerfile "${ORIGIN_SOURCE}"
+
+echo "docker push"
+docker push ${DOCKER_REPO}
+
+echo "docker stop"
+${SSH_CMD} sudo docker stop instagram
+
+echo "docker pull"
+${SSH_CMD} sudo docker pull ${DOCKER_REPO}
 
 echo "screen을 통한 서버 재시작"
 echo " - 서버 종료"
@@ -25,6 +39,6 @@ echo " - 스크린 실행"
 ${SSH_CMD} screen -S runserver -d -m
 
 echo " - 스크린에 명령어 추가"
-${SSH_CMD} "screen -r runserver -X stuff 'sudo docker run --rm -it -p 80:8000 hyeonggeun2/wps-instagram\n'"
+${SSH_CMD} "screen -r runserver -X stuff 'sudo docker run --rm -it -p 80:8000 --name=instagram hyeonggeun2/wps-instagram\n'"
 
 echo "== Docker를 통한 배포완료! =="
